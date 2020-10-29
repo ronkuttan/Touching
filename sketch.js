79a1e4abaed88a2a00c5305c2a07160edf969019
ronkuttan <72792790+ronkuttan@users.noreply.github.com>
2020-10-29 15:50:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400,400,50,80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(600,400,80,50);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  
+}
+
+function draw() {
+  background(0);  
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+  console.log(movingRect.x-fixedRect.x);
+  if(movingRect.x-fixedRect.x < movingRect.width/2 + fixedRect.width/2 &&
+    fixedRect.x-movingRect.x < movingRect.width/2 + fixedRect.width/2 &&
+    movingRect.y-fixedRect.y < movingRect.height/2 + fixedRect.height/2 &&
+    fixedRect.y-movingRect.y < movingRect.height/2 + fixedRect.height/2  ){
+    fixedRect.shapeColor = "red";
+    movingRect.shapeColor = "red";
+  }else{
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+
+  drawSprites();
+}
